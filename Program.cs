af84b4b4e8d1271751397e3543f7a029395e113b
Pedro Henrique Moreira <163483180+PedroHenriquMoreira@users.noreply.github.com>
2024-09-30 20:27:52 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,8 +10,48 @@ namespace PraticaVetor
     {
         static void Main(string[] args)
         {
+            string opcao = "";
+            Paciente cliente = new Paciente();
 
-            Console.ReadKey();
+            while (opcao.ToLower() != "q")
+            {
+                Console.Clear();
+                Console.WriteLine("1. Cadastrar paciente");
+                Console.WriteLine("2. Cadastrar paciente prioritário");
+                Console.WriteLine("3. Ver fila de espera");
+                Console.WriteLine("4. Atender paciente");
+
+                Console.WriteLine("Tecle 'q' para sair.");
+                opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        Console.Clear();
+                        cliente.cadastrarPaciente();
+                        Console.Clear();
+                        break;
+                    case "2":
+                        Console.Clear();
+                        cliente.cadastrarPrioritario();
+                        Console.Clear();
+                        break;
+                    case "3":
+                        Console.Clear();
+                        cliente.listarPaciente();
+                        Console.Clear();
+                        break;
+                    case "4":
+                        Console.Clear();
+                        cliente.atenderPaciente();
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("Escolha uma opção válida");
+                        break;
+                }
+            }
+        
         }
     }
 }
